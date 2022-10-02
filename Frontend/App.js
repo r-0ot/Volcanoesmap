524b0219da07e09484fc4627ab916d45a24b3865
Rohini <rohini.thiru@gmail.com>
2022-10-02 21:14:17 +0530

    hooks added

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,17 +1,28 @@
 import * as React from 'react';
-import { useState, useMemo} from 'react';
+import { useState, useMemo, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import Map from 'react-map-gl';
 import VOLCANOES from './volcanoes.json';
-import { Marker } from 'react-map-gl';
+import { Marker , 
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl} from 'react-map-gl';
+import'mapbox-gl/dist/mapbox-gl.css';
+import { LOAD_VOLCANOES } from './actions';
 
 export default function App() {
+  const volcanoes = useSelector((state) => state.response);
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(LOAD_VOLCANOES), []);
+  setTimeout(console.log(volcanoes), 10000);
   let [viewport, setViewPort] = useState({ longitude: VOLCANOES.features[0].properties.Longitude,
     latitude: VOLCANOES.features[0].properties.Latitude,
     zoom: 8,
    });
    const pins = useMemo(
     () =>
-      VOLCANOES.features.map((volcano, index) => (
+      volcanoes.map((volcano, index) => (
         <Marker
           key={`marker-${index}`}
           longitude={volcano.properties.Longitude}
@@ -23,13 +34,21 @@ export default function App() {
     []
   );
   return (
+    <>
+    {volcanoes && 
     <Map
+    {...viewport}
       onMove = {(newview) => setViewPort(newview)}
       mapboxAccessToken = 'pk.eyJ1Ijoicm8tMHQiLCJhIjoiY2w4bmN0bnp3MDRlbzQxbzQwYnM3bWJ3cCJ9.KmMoMoWQSHnCdTIJORS55A'
       style={{width: window.innerWidth, height: window.innerHeight}}
       mapStyle="mapbox://styles/mapbox/dark-v9"
     >
+      <GeolocateControl position="top-left" />
+        <FullscreenControl position="top-left" />
+        <NavigationControl position="top-left" />
+        <ScaleControl />
       {pins}
-    </Map>
+    </Map>}
+    </>
   );
 }
