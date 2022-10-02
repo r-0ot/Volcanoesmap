d8b5b3a11e06d9191ab7dbad858150a18bdcd565
Rohini <rohini.thiru@gmail.com>
2022-10-02 19:12:16 +0530

    filters and sorting added

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const volcanoes = require("./volcanoes.json");
+var cors = require("cors");
 const {volcanoModel} = require("./model");
 
 const app = express();
 
+app.use(cors({origin : ['http://localhost:3000'], credentials: true}));
+
 app.get("/volcanoes", function(req, res) {
+    var country = req.query.country;
+    var region = req.query.region;
+    var subregion = req.query.subregion;
+    var sortBy = req.query.sortBy;
+    var pageSize = req.query.pageSize;
+    var pageNo = req.query.pageNo;
+    var peifrom = req.query.peifrom;
+    var peito = req.query.peito;
+    var filterBy = req.query.filterBy;
+    if( country && region && subregion)
+    volcanoModel.find({Country : country, Region: region ,SubRegion: subregion }).skip((pageNo - 1)*pageSize).limit(pageSize)
+                .then(data => res.send(data))
+                .catch(err => res.status(400));
+    else if (sortBy == "VolcanonameAtoZ")
+    volcanoModel.find({}).sort({V_Name: 1}).skip((pageNo - 1)*pageSize).limit(pageSize)
+                .then(data => res.send(data))
+                .catch(err => res.status(400));
+    else if (sortBy == "VolcanonameZtoA")
+    volcanoModel.find({}).sort({V_Name: -1}).skip((pageNo - 1)*pageSize).limit(pageSize)
+                .then(data => res.send(data))
+                .catch(err => res.status(400));
+    else if (filterBy == "NoofvolcanoesbyCountry")
+    volcanoModel.aggregate({$group: {_id: "$Country", count: {$sum : 1} }},{$sort: {count: 1}}, {$skip: (pageNo - 1)*pageSize},{$limit : pageSize} )
+                .then(data => res.send(data))
+                .catch(err => res.status(400));
+    else if(peifrom && peito)
+    volcanoModel.find({PEI : {$gt : peifrom , $lt: peito}}).sort({V_Name: -1}).skip((pageNo - 1)*pageSize).limit(pageSize)
+                .then(data => res.send(data))
+                .catch(err => res.status(400));
+    else
     volcanoModel.find({})
                  .then(data => res.send(data));
 })
