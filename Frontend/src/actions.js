ff4c30dcc5a5b74a0ad48700cc9689668c19d447
Rohini <rohini.thiru@gmail.com>
2022-10-12 03:26:14 +0530

    new action added

--- Frontend/src/actions.js
+++ Frontend/src/actions.js
@@ -1,4 +1,9 @@
  export const LOAD_VOLCANOES = {type: 'LOAD_VOLCANOES'};
+ export const loadPeiData = (peifrom,peito) => ({
+   type: 'LOAD_PEI_DATA',
+   peifrom,
+   peito
+})
  export const loadVolcanoesSuccess = (payload) => ({
     type: 'LOAD_VOLCANOES_SUCCESS',
     payload
