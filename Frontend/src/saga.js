0697254e87446a60f85961b4eaea7e13fed1edb6
Rohini <rohini.thiru@gmail.com>
2022-10-12 03:39:15 +0530

    new saga implementation

--- Frontend/src/saga.js
+++ Frontend/src/saga.js
@@ -1,10 +1,15 @@
-import  {call, put, take} from "redux-saga/effects";
+import  {put, call, take} from "redux-saga/effects";
 import  {loadVolcanoesSuccess, loadVolcanoesFailure} from "./actions";
 import axios from 'axios';
 
-const fetchVolcanoes = async () => {
+const fetchVolcanoes = async (action) => {
     try{
-   const response = await axios.get('http://localhost:5000/volcanoes');
+      if(action.peifrom && action.peito){
+   const response = await axios.get(`http://localhost:5000/volcanoes?peifrom=${action.peifrom}&peito=${action.peito}`);
+   console.log(action);
+   return response.data;
+      }
+      const response = await axios.get('http://localhost:5000/volcanoes');
    return response.data;
     }
   catch(err){
@@ -14,10 +19,20 @@ const fetchVolcanoes = async () => {
 
 export function *volcanoRequest(){
     try{
-    yield take('LOAD_VOLCANOES');
-    const volcanoData = yield call(fetchVolcanoes);
+    const action = yield take('LOAD_VOLCANOES');
+    const volcanoData = yield call(fetchVolcanoes,action);
     yield put (loadVolcanoesSuccess(volcanoData));
     }catch(error){
     yield put (loadVolcanoesFailure(error.message));
     }
+}
+
+export function *peiDataRequest(){
+  try{
+  const action = yield take('LOAD_PEI_DATA');
+  const peiData = yield call(fetchVolcanoes, action);
+  yield put (loadVolcanoesSuccess(peiData));
+  }catch(error){
+  yield put (loadVolcanoesFailure(error.message));
+  }
 }
